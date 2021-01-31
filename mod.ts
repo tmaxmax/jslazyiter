b21f1e49144ba9304de743dc275cb2c3ddde8d3f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-31 13:39:11 +0200

    Use Iterator instead of Iterable under the hood

--- mod.ts
+++ mod.ts
@@ -1,7 +1,17 @@
 export default class Iter<T> implements Iterable<T> {
-  constructor(private readonly iter: Iterable<T>) {}
+  private readonly iter: Iterator<T>
+
+  constructor(iterable: Iterable<T>)
+  constructor(iterator: Iterator<T>)
+  constructor(iter: Iterator<T> | Iterable<T>) {
+    if (Symbol.iterator in iter) {
+      this.iter = (iter as Iterable<T>)[Symbol.iterator]()
+    } else {
+      this.iter = iter as Iterator<T>
+    }
+  }
 
   [Symbol.iterator]() {
-    return this.iter[Symbol.iterator]();
+    return this.iter
   }
 }
