dcdcaaa3f9f94d9b843a3a8a3a05e421e5e54628
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-01-31 14:52:27 +0200

    Format file

--- mod.ts
+++ mod.ts
@@ -1,17 +1,17 @@
 export class Iter<T> implements Iterable<T> {
-  private readonly iter: Iterator<T>
+  private readonly iter: Iterator<T>;
 
-  constructor(iterable: Iterable<T>)
-  constructor(iterator: Iterator<T>)
+  constructor(iterable: Iterable<T>);
+  constructor(iterator: Iterator<T>);
   constructor(iter: Iterator<T> | Iterable<T>) {
     if (Symbol.iterator in iter) {
-      this.iter = (iter as Iterable<T>)[Symbol.iterator]()
+      this.iter = (iter as Iterable<T>)[Symbol.iterator]();
     } else {
-      this.iter = iter as Iterator<T>
+      this.iter = iter as Iterator<T>;
     }
   }
 
   [Symbol.iterator]() {
-    return this.iter
+    return this.iter;
   }
 }
