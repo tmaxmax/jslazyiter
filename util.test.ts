88bdc4fd849fca0a62de6bc2d86729490925b1ea
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-02-01 18:54:26 +0200

    Create utility function for comparing numbers

--- util.test.ts
+++ util.test.ts
@@ -1,8 +1,25 @@
-import { parseIntegral } from "./util.ts";
+import { cmpNumbers, parseIntegral } from "./util.ts";
 import { none, some } from "./option.ts";
 import { assert } from "https://deno.land/std@0.85.0/testing/asserts.ts";
 
 Deno.test("utilities: parseIntegral", () => {
   assert(some(parseIntegral("12345")));
   assert(none(parseIntegral("sarmale")));
+});
+
+Deno.test("utilities: cmpNumbers", () => {
+  {
+    const res = cmpNumbers(1, 1);
+    assert(some(res) && res === 0);
+  }
+  {
+    const res = cmpNumbers(1, 2);
+    assert(some(res) && res < 0);
+  }
+  {
+    const res = cmpNumbers(43, 21);
+    assert(some(res) && res > 0);
+  }
+  assert(none(cmpNumbers(1, NaN)));
+  assert(none(cmpNumbers(NaN, 32)));
 });
